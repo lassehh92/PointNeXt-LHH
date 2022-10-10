14a71bb7d0da343a365982421cd0fd7c4c8e3461
Lasse Hansen <lasse92@me.com>
2022-10-10 14:33:02 +0200

    vis_s3dis-results update

--- examples/segmentation/vis_s3dis_results.py
+++ examples/segmentation/vis_s3dis_results.py
@@ -4,15 +4,22 @@ from glob import glob
 from openpoints.dataset.vis3d import read_obj, vis_multi_points, vis_points
 
 #### names change the name
+#methods_dir = [
+#    'pretrained/s3dis/PointNet++original/s3dis-train-pointnet++original-ngpus1-seed5123-20220515-214346-kKe7d9yNdMrnUoLxsYcpCm/visualization', 
+#    'pretrained/s3dis/PointNeXt-XL/s3disfull-train-pointnext-XL-ngpus1-seed4464-batch_size=8-20220425-014658-7Ew4sJwdKppnWsS6UW2LrA/visualization'
+#    ]
 methods_dir = [
-    'pretrained/s3dis/PointNet++original/s3dis-train-pointnet++original-ngpus1-seed5123-20220515-214346-kKe7d9yNdMrnUoLxsYcpCm/visualization', 
-    'pretrained/s3dis/PointNeXt-XL/s3disfull-train-pointnext-XL-ngpus1-seed4464-batch_size=8-20220425-014658-7Ew4sJwdKppnWsS6UW2LrA/visualization'
-    ]
-prefix_names = ['input-Area5-', 'gt-Area5-', 'pred-Area5-']
-idx =  45 
+	'log/s3dis/s3dis-train-pointnext-xl-ngpus1-seed1045-20221008-170028-9LKswMZCFEnShmwcCgsFdY_ckpt_best.pth_20221008-233159-CiCAoPyMoVReUYhFcoHAM4/visualization'
+	]
+#prefix_names = ['input-Area5-', 'gt-Area5-', 'pred-Area5-']
+#idx =  45 
 # good: 3, 4, 17, 40, 55
 # fair: 30
 # 65
+
+prefix_names = ['input-s3dis-Area5-','gt-s3dis-Area5-','pred-s3dis-Area5-']
+idx = 3
+
 file_list = glob(osp.join(methods_dir[0], '*'))
 
 save_dir = 'pretrained/s3dis/visualization/'
@@ -25,5 +32,6 @@ valid_idx = input_points[:, 2] < 3
 input_points = input_points[valid_idx]
 gt_points, gt_colors =read_obj(osp.join(methods_dir[0], prefix_names[1]+str(idx)+'.obj'))
 method1_points, method1_colors =read_obj(osp.join(methods_dir[0], prefix_names[2]+str(idx)+'.obj'))
-method2_points, method2_colors =read_obj(osp.join(methods_dir[1], prefix_names[2]+str(idx)+'.obj'))
+#method2_points, method2_colors =read_obj(osp.join(methods_dir[1], prefix_names[2]+str(idx)+'.obj'))
 vis_multi_points([input_points, input_points, input_points, input_points], [input_colors[valid_idx],  method1_colors[valid_idx], method2_colors[valid_idx], gt_colors[valid_idx]])
+#vis_single_pointCloud()
